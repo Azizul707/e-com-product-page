1e96a4ffe66ee77b469712ba672edc4c4cf88814
Azizul <arafinazizul@gmail.com>
2023-08-20 23:28:33 +0600

    some modification on html and style.css file for resplonsive

--- js/utilities.js
+++ js/utilities.js
@@ -1,5 +1,5 @@
 
-let total = 0;
+let total = 0
 function getProductNameAndPrice(event){
     const productName = event.childNodes[3].childNodes[3].innerText;
     const productList = document.getElementById('items-field');
@@ -9,4 +9,7 @@ function getProductNameAndPrice(event){
     productList.appendChild(p);
     const price = event.childNodes[3].childNodes[5].innerText.split(' ')[0];
     console.log(price);
+    const totalPrice = document.getElementById('total-price').innerText.split(' ')[0];
+    total = (parseFloat(totalPrice) + parseFloat(price));
+    console.log(totalPrice);
 }
